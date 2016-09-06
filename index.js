ed53cc7ed039aac53768d1668cb7dec7cc9c277a
Anton Zhyravlev <zhyravlev.as@gmail.com>
2016-09-06 17:34:16 +0300

    Init version 0.0.2

--- index.js
+++ index.js
@@ -17,8 +17,24 @@
       var def = $q.defer();
       var jGrowlOptions = angular.element.jGrowl.defaults;
 
-      var show = function (message, theme) {
+      var show = function (a, theme) {
+        var message, header;
+        switch (a.length) {
+          case 1:
+            message = a[0];
+            header = '';
+            break;
+          case 2:
+            header = a[0];
+            message = a[1];
+            break;
+          default:
+            def.reject();
+            return def.promise;
+        }
+
         angular.element.jGrowl(message, {
+          header: header,
           themeState: theme,
           close: function () {
             def.resolve();
@@ -31,20 +47,20 @@
       return function (options) {
         angular.extend(jGrowlOptions, options || {});
 
-        this.success = function (message) {
-          return show(message, 'success')
+        this.success = function () {
+          return show(arguments, 'success')
         };
 
-        this.error = function (message) {
-          return show(message, 'error')
+        this.error = function () {
+          return show(arguments, 'error')
         };
 
-        this.warn = function (message) {
-          return show(message, 'warn')
+        this.warn = function () {
+          return show(arguments, 'warn')
         };
 
-        this.info = function (message) {
-          return show(message, 'info')
+        this.info = function () {
+          return show(arguments, 'info')
         };
       };
     }]);
